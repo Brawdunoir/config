a21651e1bda63476e643e9ea6411bc7ca678ad31
Yann Lacroix <yann.lacroix@advans-group.com>
2025-05-19 15:23:56 +0200

    fix greeting

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
+function fish_greeting
+    echo I am not as think as you drunk I am
+end
+
 export EDITOR=micro
 starship init fish | source
 export EDITOR=micro
--- fish/functions/greeting.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting
-    echo I am not as think as you drunk I am
-end
